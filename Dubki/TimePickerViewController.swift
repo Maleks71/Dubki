e2eb04d905546e8acdc6d425ea9110785d59de70
Моренко Игорь Леонидович <morenko@lionsoft.ru>
2015-11-15 11:33:56 +0300

    Добавлено ограничение выбора времени +30 дней.

--- Dubki/TimePickerViewController.swift
+++ Dubki/TimePickerViewController.swift
@@ -24,6 +24,7 @@ class TimePickerViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         datePicker.minimumDate = NSDate()
+        datePicker.maximumDate = NSDate().dateByAddingDay(30)
         if selectedDate != nil {
             datePicker.date = selectedDate!
         }
